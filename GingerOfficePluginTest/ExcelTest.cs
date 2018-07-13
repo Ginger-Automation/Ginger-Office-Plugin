17e027d956f1cfb567a1003bf54c0490bd29106d
Mahesh Shahane <mahessha@amdocs.com>
2018-07-13 17:18:22 +0530

    Created new branch from master and Added more actions for various operations on excel files with OpenXML library

--- GingerOfficePluginTest/ExcelTest.cs
+++ GingerOfficePluginTest/ExcelTest.cs
@@ -21,31 +21,17 @@ using GingerTestHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StandAloneActions;
 using System;
-using System.Reflection;
 
 namespace StandAloneActionsTest
 {
-
-    [TestClass]    
+    [TestClass]
     public class ExcelTest
-    {        
-
-        static string EXCEL_FILE_NAME = TestResources.GetTestResourcesFile("test1.xlsx");
-      
-
-        [ClassInitialize]
-        public static void ClassInit(TestContext context)
-        {
-            if (!System.IO.File.Exists(EXCEL_FILE_NAME))
-            {
-                Console.WriteLine("File not found: " + EXCEL_FILE_NAME);
-            }
-        }
-
+    {
+        string EXCEL_FILE_NAME = TestResources.GetTestResourcesFile("test1.xlsx");
+           
         [TestMethod]
         public void ReadExcelCellRow3ColumnB()
-        {            
-
+        {
             //Arrange
             ExcelAction x = new ExcelAction();
             GingerAction GA = new GingerAction("Excel");
@@ -54,7 +40,7 @@ namespace StandAloneActionsTest
             x.ReadExcelCell(ref GA, EXCEL_FILE_NAME, "Sheet1", "#3", "#B");
 
             //Assert
-            // Assert.AreEqual("Moshe", GA.Output.Values[0].ValueString, "Row 3 Col B = Moshe");
+            Assert.AreEqual("Moshe", GA.Output.Values[0].ValueString, "Row 3 Col B = Moshe");
         }
 
         [TestMethod]
@@ -68,9 +54,185 @@ namespace StandAloneActionsTest
             x.ReadExcelCell(ref GA, EXCEL_FILE_NAME, "Sheet1", "#3", "#B");
 
             //Assert
-            // Assert.AreEqual(1, GA.Output.Values.Count);
+            Assert.AreEqual(1, GA.Output.Values.Count);
         }
 
-     
+        [TestMethod]
+        public void ReadExcelCellWhereFirstEquelMosheGetID()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelCell(ref GA, EXCEL_FILE_NAME, "Sheet1", "First='Moshe'", "ID");
+            
+            //Assert            
+            Assert.AreEqual("24", GA.Output.Values[0].ValueString, "First=Moshe ID=24");
+        }
+
+        [TestMethod]
+        public void ReadExcelRow2()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelRow(ref GA, EXCEL_FILE_NAME, "Sheet1", "#2", string.Empty);     // if no columns are specified read them all       
+            
+            //Assert
+            Assert.AreEqual("12", GA.Output.Values[0].ValueString, "Row 2 ID=12");
+            Assert.AreEqual("David", GA.Output.Values[1].ValueString, "Row 2 First=David");
+            Assert.AreEqual("Cohen", GA.Output.Values[2].ValueString, "Row 2 Last=Cohen");
+            Assert.AreEqual("923646", GA.Output.Values[3].ValueString, "Row 2 Phone=923646");
+            Assert.AreEqual("Yes", GA.Output.Values[4].ValueString, "Row 2 Used=Yes");
+        }
+        
+        [TestMethod]
+        public void ReadExcelRow3GetColumns_1_2_4()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelRow(ref GA, EXCEL_FILE_NAME, "Sheet1", "#3","#1,#2,#4");  // Read row 3 columns 1,2,4
+            
+            //Assert            
+            Assert.AreEqual("24", GA.Output.Values[0].ValueString, "Row 3 ID=24");
+            Assert.AreEqual("Moshe", GA.Output.Values[1].ValueString, "Row 3 First=Moshe");
+            Assert.AreEqual("73769", GA.Output.Values[2].ValueString, "Row 3 Phone=73769");            
+        }
+
+        [TestMethod]
+        public void ReadExcelRow3GetColumnsCount()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelRow(ref GA, EXCEL_FILE_NAME, "Sheet1", "#3", "#1,#2,#4");  // Read row 3 columns 1,2,4
+                                                                        //Assert
+            Assert.AreEqual(3, GA.Output.Values.Count);
+        }
+
+        [TestMethod]
+        public void ReadExcelCellWhereUsedIsNoAndGetID()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelRow(ref GA, EXCEL_FILE_NAME, "Sheet1", "Used='No'", "ID");  // Read first row where Used = No and get the ID
+
+            //Assert
+            Assert.AreEqual("24", GA.Output.Values[0].ValueString, "Used=No is ID=24");
+        }
+
+        [TestMethod]
+        public void ReadExcelRowWithConditionIDMoreThan30AndUsedIsNo()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelRow(ref GA, EXCEL_FILE_NAME, "Sheet1", "ID>'30' and Used='No'", "#A, #2, Phone, #5");   // Read the first row when ID>30 and Used=No, get column: A,B,D,E
+
+            //Assert
+            Assert.AreEqual("32", GA.Output.Values[0].ValueString, "Row 4 ID=32");
+            Assert.AreEqual("Dana", GA.Output.Values[1].ValueString, "Row 4 First=Dana");
+            Assert.AreEqual("878375", GA.Output.Values[2].ValueString, "Row 4 Phone=878375");
+            Assert.AreEqual("No", GA.Output.Values[3].ValueString, "Row 4 Used=No");
+        }
+
+        [TestMethod]
+        public void ReadUpdatePassExcelCellWithRowNoGetColumnName()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelAndUpdate(ref GA, EXCEL_FILE_NAME, "Sheet1", "#3", "#1,#2,#4", "ID='12', #C='John', First='Dave'");      //1   John is optional
+
+            //Assert    
+            Assert.AreEqual("24", GA.Output.Values[0].ValueString, "Row 3 ID=24");
+            Assert.AreEqual("Moshe", GA.Output.Values[1].ValueString, "Row 3 First=Moshe");
+            Assert.AreEqual("73769", GA.Output.Values[2].ValueString, "Row 3 Phone=73769");
+        }
+
+        [TestMethod]
+        public void ReadUpdateFailExcelCellWithRowNo()
+        {
+            ///Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.ReadExcelAndUpdate(ref GA, EXCEL_FILE_NAME, "Sheet1", "#5", string.Empty, "ID=5, #C='Thomas', First='Sarah'");
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(GA.Errors));
+        }
+        
+        [TestMethod]
+        public void AppendNewRowSimple()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+            
+            x.AppendData(ref GA, EXCEL_FILE_NAME, "Sheet1", "'55', 'John', 'Smith'");
+            int index = Convert.ToInt32(GA.Output.Values[0].ValueString);
+            
+            //Assert
+            Assert.IsTrue(index > 0);
+        }
+
+        [TestMethod]
+        public void AppendNewRowByColumnHeading()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            x.AppendData(ref GA, EXCEL_FILE_NAME, "Sheet1", "Last='aaa', Used='No'");
+            int index = Convert.ToInt32(GA.Output.Values[0].ValueString);
+
+            //Assert
+            Assert.IsTrue(index > 0);
+        }
+
+        [TestMethod]
+        public void AppendNewRowByColumnName()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            x.AppendData(ref GA, EXCEL_FILE_NAME, "Sheet1", "#B='ColName', #E='Yes'");
+            int index = Convert.ToInt32(GA.Output.Values[0].ValueString);
+
+            //Assert
+            Assert.IsTrue(index > 0);
+        }
+
+        [TestMethod]
+        public void WriteExcelPass()
+        {
+            //Arrange
+            ExcelAction x = new ExcelAction();
+            GingerAction GA = new GingerAction("Excel");
+
+            //Act
+            x.WriteExcel(ref GA, EXCEL_FILE_NAME, "Sheet1", 7, "B", "Write Cell");
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(GA.Errors));
+        }
     }
 }
